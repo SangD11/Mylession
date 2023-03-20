56e551c0fbfc14b9c889c9910f59d33a6050135f
Sangid11 <sangeeta0309@yahoo.com>
2023-03-20 15:02:11 -0400

    Add

--- src/main/java/javaLenght.java
+++ src/main/java/javaLenght.java
@@ -2,5 +2,7 @@ public class javaLenght {
     public static void main(String[] args) {
         String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         System.out.println("The length of the txt string is: " + txt.length());
+
+        //the lenght of char
     }
 }
